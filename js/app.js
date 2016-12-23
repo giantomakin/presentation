e417fdffddc18ac7f7b51b59a8f7ce9bc3607fd1
giantomakin <gian@marble.com>
2016-12-23 13:43:02 -0500

    added color function

--- js/app.js
+++ js/app.js
@@ -2,64 +2,71 @@ var app = angular.module('presentationApp', []);
 app.controller('mainController', function($scope, $browser) {
 
 	$scope.baseURL = window.location.pathname;
-  console.log(window.location.pathname);
+
+	$scope.pickPallete = function(e){
+		e.preventDefault();
+		var color = $(e.target).data('color');
+		alert(color);
+	}
+
 
  });
 
-var $item = $('.carousel .item');
-var $wHeight = $(window).height();
-var myCarousel = $('#mycarousel');
-var myNav = myCarousel.prev();
+$(document).ready(function(){
+	var $item = $('.carousel .item');
+	var $wHeight = $(window).height();
+	var myCarousel = $('#mycarousel');
+	var myNav = myCarousel.prev();
 
-$item.eq(0).addClass('active');
-$item.height($wHeight);
-$item.addClass('full-screen');
-$("#a1").addClass("itemSelected");
-$("#a1").prev().addClass("makeVisible");
+	$item.eq(0).addClass('active');
+	$item.height($wHeight);
+	$item.addClass('full-screen');
+	$("#a1").addClass("itemSelected");
+	$("#a1").prev().addClass("makeVisible");
 
-$(window).on('resize', function (){
-  $wHeight = $(window).height();
-  $item.height($wHeight);
+	$(window).on('resize', function (){
+	  $wHeight = $(window).height();
+	  $item.height($wHeight);
+	});
+
+	myNav.find('li > a').click(function() {
+	  var id = $(this).attr('id');
+	  var newIndex = $(this).parent().index();
+	  myCarousel.carousel( newIndex );
+
+	  $(".nav-a").removeClass("itemSelected");
+	  $(".nav-a").prev().removeClass("makeVisible");
+
+	  switch(id){
+	  	case "a1":
+	  		$("#a1").addClass("itemSelected");
+	  		$("#a1").prev().addClass("makeVisible");
+	  	break;
+	  	case "a2":
+	  		$("#a2").addClass("itemSelected");
+	  		$("#a2").prev().addClass("makeVisible");
+	  	break;
+	  	case "a3":
+	  		$("#a3").addClass("itemSelected");
+	  		$("#a3").prev().addClass("makeVisible");
+	  	break;
+	  	case "a4":
+	  		$("#a4").addClass("itemSelected");
+	  		$("#a4").prev().addClass("makeVisible");
+	  	break;
+	  	case "a5":
+	  		$("#a5").addClass("itemSelected");
+	  		$("#a5").prev().addClass("makeVisible");
+	  	break;
+	  	case "a6":
+	  		$("#a6").addClass("itemSelected");
+	  		$("#a6").prev().addClass("makeVisible");
+	  	break;
+	  }
+
+	});
+
 });
-
-myNav.find('li > a').click(function() {
-  var id = $(this).attr('id');
-  var newIndex = $(this).parent().index();
-  myCarousel.carousel( newIndex );
-
-  $(".nav-a").removeClass("itemSelected");
-  $(".nav-a").prev().removeClass("makeVisible");
-
-  switch(id){
-  	case "a1":
-  		$("#a1").addClass("itemSelected");
-  		$("#a1").prev().addClass("makeVisible");
-  	break;
-  	case "a2":
-  		$("#a2").addClass("itemSelected");
-  		$("#a2").prev().addClass("makeVisible");
-  	break;
-  	case "a3":
-  		$("#a3").addClass("itemSelected");
-  		$("#a3").prev().addClass("makeVisible");
-  	break;
-  	case "a4":
-  		$("#a4").addClass("itemSelected");
-  		$("#a4").prev().addClass("makeVisible");
-  	break;
-  	case "a5":
-  		$("#a5").addClass("itemSelected");
-  		$("#a5").prev().addClass("makeVisible");
-  	break;
-  	case "a6":
-  		$("#a6").addClass("itemSelected");
-  		$("#a6").prev().addClass("makeVisible");
-  	break;
-  }
-
-});
-
-
 
 
 
