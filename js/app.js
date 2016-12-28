cd1e0035c007f2ff13a842de8aad98025b98db90
giantomakin <gian@marble.com>
2016-12-28 09:48:05 -0500

    optimized slider code

--- js/app.js
+++ js/app.js
@@ -30,75 +30,22 @@ $(document).ready(function(){
 	});
 
 	myCarousel.bind('slide.bs.carousel', function (e) {
-
+		var id = $(e.relatedTarget).data('target-id');
 	    $(".nav-a").removeClass("itemSelected");
 	    $(".nav-a").prev().removeClass("makeVisible");
-
-	    switch($(e.relatedTarget).attr('id')){
-	    	case "item1":
-	    		$("#a1").addClass("itemSelected");
-	    		$("#a1").prev().addClass("makeVisible");
-	    	break;
-	    	case "item2":
-	    		$("#a2").addClass("itemSelected");
-	    		$("#a2").prev().addClass("makeVisible");
-	    	break;
-	    	case "item3":
-	    		$("#a3").addClass("itemSelected");
-	    		$("#a3").prev().addClass("makeVisible");
-	    	break;
-	    	case "item4":
-	    		$("#a4").addClass("itemSelected");
-	    		$("#a4").prev().addClass("makeVisible");
-	    	break;
-	    	case "item5":
-	    		$("#a5").addClass("itemSelected");
-	    		$("#a5").prev().addClass("makeVisible");
-	    	break;
-	    	case "item6":
-	    		$("#a6").addClass("itemSelected");
-	    		$("#a6").prev().addClass("makeVisible");
-	    	break;
-	    }
-
-
+	    $("#"+id).addClass("itemSelected");
+	    $("#"+id).prev().addClass("makeVisible");
 	});
 
 	myNav.find('li > a').click(function() {
 	  var id = $(this).attr('id');
 	  var newIndex = $(this).parent().index();
 	  myCarousel.carousel( newIndex );
-
 	  $(".nav-a").removeClass("itemSelected");
 	  $(".nav-a").prev().removeClass("makeVisible");
 
-	  switch(id){
-	  	case "a1":
-	  		$("#a1").addClass("itemSelected");
-	  		$("#a1").prev().addClass("makeVisible");
-	  	break;
-	  	case "a2":
-	  		$("#a2").addClass("itemSelected");
-	  		$("#a2").prev().addClass("makeVisible");
-	  	break;
-	  	case "a3":
-	  		$("#a3").addClass("itemSelected");
-	  		$("#a3").prev().addClass("makeVisible");
-	  	break;
-	  	case "a4":
-	  		$("#a4").addClass("itemSelected");
-	  		$("#a4").prev().addClass("makeVisible");
-	  	break;
-	  	case "a5":
-	  		$("#a5").addClass("itemSelected");
-	  		$("#a5").prev().addClass("makeVisible");
-	  	break;
-	  	case "a6":
-	  		$("#a6").addClass("itemSelected");
-	  		$("#a6").prev().addClass("makeVisible");
-	  	break;
-	  }
-
+	  $("#"+id).addClass("itemSelected");
+	  $("#"+id).prev().addClass("makeVisible");
 	});
 
 });
