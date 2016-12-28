648f969647ec85427bf0b7a3573a23de9d7d8467
giantomakin <gian@marble.com>
2016-12-28 09:41:11 -0500

    fixed slide

--- js/app.js
+++ js/app.js
@@ -29,6 +29,41 @@ $(document).ready(function(){
 	  $item.height($wHeight);
 	});
 
+	myCarousel.bind('slide.bs.carousel', function (e) {
+
+	    $(".nav-a").removeClass("itemSelected");
+	    $(".nav-a").prev().removeClass("makeVisible");
+
+	    switch($(e.relatedTarget).attr('id')){
+	    	case "item1":
+	    		$("#a1").addClass("itemSelected");
+	    		$("#a1").prev().addClass("makeVisible");
+	    	break;
+	    	case "item2":
+	    		$("#a2").addClass("itemSelected");
+	    		$("#a2").prev().addClass("makeVisible");
+	    	break;
+	    	case "item3":
+	    		$("#a3").addClass("itemSelected");
+	    		$("#a3").prev().addClass("makeVisible");
+	    	break;
+	    	case "item4":
+	    		$("#a4").addClass("itemSelected");
+	    		$("#a4").prev().addClass("makeVisible");
+	    	break;
+	    	case "item5":
+	    		$("#a5").addClass("itemSelected");
+	    		$("#a5").prev().addClass("makeVisible");
+	    	break;
+	    	case "item6":
+	    		$("#a6").addClass("itemSelected");
+	    		$("#a6").prev().addClass("makeVisible");
+	    	break;
+	    }
+
+
+	});
+
 	myNav.find('li > a').click(function() {
 	  var id = $(this).attr('id');
 	  var newIndex = $(this).parent().index();
